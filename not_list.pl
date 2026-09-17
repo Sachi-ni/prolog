419e940aac2ba8ae897cb3ba5bc08828248d1f45
Weerasingha R.P.G.S.N <nisansalaweerasingha3848@gmail.com>
2026-09-17 14:07:57 +0530

    Update not_list.pl

--- not_list.pl
+++ not_list.pl
@@ -11,3 +11,40 @@ not_bird(X) :- \+bird(X).
                           1true
                           not_bird(eagle).
                           false
+
+                          
+member(X, [X|_]).
+member(X, [_|T]) :- member(X, T).
+
+not_in_list(X, L) :- \+member(X, L).
+
+                                        not_in_list(10,[1,2,3]).
+                                        1true
+
+likes(mary, pizza).
+likes(john, pizza).
+
+                    likes(mary, pizza); likes(john, pizza). // ';'- or
+                    1true
+                    2true
+                    
+                    likes(mary, pizza), likes(john, pizza). //','- and
+                    1true
+
+
+          (Result = a; Result= b), Result = b.
+          Result = b
+          (Result = a; Result= b), !, Result = b.
+          false
+          
+          member(X, [a,b,c]).
+          X = a
+          X = b
+          X = c
+          member(X, [a,b,c]), !.
+          X = a
+          member(X, [a,b,c]), X=b.
+          X = b
+          false
+          member(X, [a,b,c]), !, X=b.
+          false
